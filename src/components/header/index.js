a3b396df5bd1d69ded21d8656c77dd6a686a888e
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2019-09-15 12:48:21 -0500

    header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,7 +7,7 @@ const Header = () => (
   <header className="header">
 
     <div className="header-banner">
-      <img src="icons/banner-jsconfco-2018.jpg" alt="Logo JSConf colombia"/>
+      <img src="../../assets/images/banner_jsconfco_2018.jpg" alt="Logo JSConf colombia"/>
     </div>
 
     <div className="container">
@@ -15,7 +15,7 @@ const Header = () => (
       <div className="row  content-information">
 
         <div className="col  content-logo">
-          <img src="./icons/Logo-JSConco-2019.svg" alt="Logo JSConf colombia"/>
+          <img src="../../assets/images/LogoJSConco2019.svg" alt="Logo JSConf colombia"/>
         </div>
 
         <p className="col  header-information">Bringing the Colombian JavaScript community under the roof.</p>
