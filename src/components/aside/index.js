9c23f00fa2a552eef82f627343ed0aae3f62e841
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2019-09-15 17:49:26 -0500

    delete arrow

--- src/components/aside/index.js
+++ src/components/aside/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
-// import "./scroll-menu-icon";
+
 import "./aside.scss";
 
 class Asidebar extends React.Component {
+
   render() {
+
     return (
+
+
       <aside className="aside">
+
         <div className="container">
           <div className="sidebar-btn-menu-mobile" id="sidebar-btn-menu-mobile">
             <i className="icon-menu"></i>
@@ -13,88 +18,66 @@ class Asidebar extends React.Component {
         </div>
 
         <div className="sidebar" id="sidebar">
+
           <div className="content-btn-menu">
-            <a
-              className="icon-nav  btn-close"
-              href="#"
-              rel="noopener noreferrer"
-            >
+            <a className="icon-nav  btn-close" href="#" rel="noopener noreferrer">
               <i className="icon-close"></i>
             </a>
           </div>
 
           <div className="logo-menu">
-            <a href="#">
-              <img
-                src="icons/logo-jsconfco-2019.svg"
-                alt="Logo JSConf colombia"
-              />
+            <a
+              href="#"
+            >
+              <img src="icons/logo-jsconfco-2019.svg" alt="Logo JSConf colombia"/>
             </a>
           </div>
 
           <div className="menu-nav">
             <ul className="main-menu" id="main-menu">
               <li className="main-menu__item  active">
-                <a className="main-menu__link" href="#top">
-                  top
-                </a>
+                <a className="main-menu__link" href="#top">top</a>
               </li>
               <li className="main-menu__item">
-                <a className="main-menu__link" href="#speaker">
-                  speakers
-                </a>
+                <a className="main-menu__link" href="#speaker">speakers</a>
               </li>
               <li className="main-menu__item">
-                <a className="main-menu__link" href="#sponsor">
-                  sponsors
-                </a>
+                <a className="main-menu__link" href="#sponsor">sponsors</a>
               </li>
               <li className="main-menu__item">
-                <a className="main-menu__link" href="#venue">
-                  venue
-                </a>
+                <a className="main-menu__link" href="#venue">venue</a>
               </li>
               <li className="main-menu__item">
-                <a className="main-menu__link" href="#team">
-                  team
-                </a>
+                <a className="main-menu__link" href="#team">team</a>
               </li>
               <li className="main-menu__item">
-                <a className="main-menu__link" href="#schedule">
-                  schedule
-                </a>
+                <a className="main-menu__link" href="#schedule">schedule</a>
               </li>
               <li className="main-menu__item">
-                <a className="main-menu__link" href="#tuned">
-                  stay tuned
-                </a>
+                <a className="main-menu__link" href="#tuned">stay tuned</a>
               </li>
             </ul>
           </div>
 
           <div className="social-media-menu">
             <p className="social-media-menu__description">Follow us!</p>
-            <a
-              className="icon-nav"
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a className="icon-nav" href="#" target="_blank" rel="noopener noreferrer">
               <i className="icon-instagram"></i>
             </a>
-            <a
-              className="icon-nav"
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a className="icon-nav" href="#" target="_blank" rel="noopener noreferrer">
               <i className="icon-twitter"></i>
             </a>
           </div>
+
         </div>
+
       </aside>
-    );
+
+    )
   }
+
 }
 
+
 export default Asidebar;
+
