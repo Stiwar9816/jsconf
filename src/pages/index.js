86db1c7666e72b0f3d117fea9f5dab0f5a9fca57
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2019-09-15 21:30:00 -0500

    arreglos schedule

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,12 @@ import SectionSpeakers from "../components/section_speakers";
 import SectionSponsor from "../components/section_sponsors";
 import SectionVenue from "../components/section_venue";
 import SectionTeam from "../components/section_team";
-import Asidebar from '../components/aside'
 import "./styles.scss";
 
 class IndexPage extends React.Component {
   render() {
     return (
       <Layout>
-      <Asidebar />
         <SectionSpeakers />
         <SectionSponsor />
         <SectionVenue />
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Layout from '../components/layout'
 // import Schedule from '../components/page_schedule'
 
