1352338f81483eadf9dcf4ca142c3502997f4ad5
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2019-09-16 13:39:21 -0500

    footer corregido

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,7 +6,26 @@ export default class SectionFooter extends Component {
       <footer className="footer">
         <div className="container">
           <div className="col s12">
-            <p className="footer-copyright">JSConf colombia 2019</p>
+            <p className="footer-copyright">
+              JSConfco colombia 2019 - Created by:{" "}
+              <a
+              className="underline"
+                href="https://twitter.com/asprillastiwar"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+               Stiwar Asprilla
+              </a>{" "}
+              -{" "}
+              <a
+              className="underline"
+                href="https://twitter.com/miltoncode"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Hamilton Galeano
+              </a>
+            </p>
           </div>
         </div>
       </footer>
