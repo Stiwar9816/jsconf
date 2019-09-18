1f1657ba0e6cf90bdb5376b626063944466e5ffa
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2019-09-18 00:29:46 -0500

    imagenes arregladas

--- src/components/images/team_lizparody.js
+++ src/components/images/team_lizparody.js
@@ -17,7 +17,7 @@ const Image = (props) => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "team_lizparody.jpeg" }) {
+        placeholderImage: file(relativePath: { eq: "team_lizparody.jpg" }) {
           childImageSharp {
             fluid(maxWidth: 400) {
               ...GatsbyImageSharpFluid
