d7677d57e82a927c33ecc41ce54ab73072f97f0d
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2019-09-18 23:43:58 -0500

    scss page schedule

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import '../components/layout.scss'
+import '../components/page_schedule/schedule.scss'
 import ScheduleComponent from '../components/page_schedule'
 
 class Schedule extends React.Component {
