8a5c1c4e8372846b9fd64a02eb71de744a55d064
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2019-09-20 11:30:00 -0500

    link gatsby

--- src/components/aside/index.js
+++ src/components/aside/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Link } from "react";
+import React from "react";
+import { Link } from "gatsby";
 import "./aside.scss";
 
 class Asidebar extends React.Component {
