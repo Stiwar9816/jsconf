4a632907d0f0639018aa6b7e9c6de21c731a6df1
Milton-code <milton.website@gmail.com>
2019-09-22 08:42:53 -0500

    Animacion para el menu desktop

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,38 @@ import "./styles.scss";
 
 class IndexPage extends React.Component {
 
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const offset = 130;
+
+    const position = window.pageYOffset;
+
+    // console.log(document.querySelectorAll('.aside .main-menu__item a'));
+    document.querySelectorAll('.aside .main-menu__item a').forEach(a => {
+      var anchorId = a.getAttribute('href');
+
+      if (anchorId.startsWith('#') && document.querySelector(anchorId)) {
+        const target = document.querySelector(anchorId).offsetTop - offset;
+        const targetHeight = document.querySelector(anchorId).offsetHeight + target;
+        if (position >= target && position < targetHeight) {
+          const menuItems = document.querySelectorAll('.aside .main-menu__item');
+          for (let i = 0; i < menuItems.length; i++) {
+            menuItems[i].classList.remove('active');
+          }
+          a.parentElement.classList.add('active');
+        }
+      }
+    });
+  }
+
+
   render() {
     return (
       <Layout>
